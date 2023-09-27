c808c1c2b9cb738ebebbc39291a59710c2de8f89
Alfandani araniri aditya putra <alfandnap@gmail.com>
2023-09-27 17:54:50 +0700

    Update: backend unpopulated

---
+++ Backend/app/controllers/messages_controller.rb
@@ -0,0 +1,59 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[ show update destroy ]
+
+  # GET /messages
+  def index
+    @messages = Message.all
+
+    render json: @messages
+  end
+
+  # GET /messages/1
+  def show
+    render json: @message
+  end
+
+  # POST /messages
+  def create
+    message_object = params[:body]
+    guid = message_object[:id]
+    message_text = message_object[:message]
+
+    message_data = { guid: guid, message: message_text }
+
+    @message = Message.new(body: message_data.to_json)
+
+    # @message = Message.new(message_params)
+
+    if @message.save
+      render json: @message, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /messages/1
+  def update
+    if @message.update(message_params)
+      render json: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /messages/1
+  def destroy
+    @message.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def message_params
+      params.require(:message).permit(:body)
+    end
+end
